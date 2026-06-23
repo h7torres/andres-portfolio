e3c867055765b2264670eebe11c1fa4d311388e9
h7torres <h7torres@ucsd.edu>
2026-06-22 22:17:18 -0700

    Fix BrowserRouter basename for GitHub Pages subpath

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function Contact() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
